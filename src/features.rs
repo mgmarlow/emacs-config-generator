d4fbc90f9505c0e51e8e0c94df6bba16e2115347
mgmarlow <graham@mgmarlow.com>
2023-06-06 22:23:02 -0700

    Breadcrumbs and copy updates

--- src/features.rs
+++ src/features.rs
@@ -1,8 +1,8 @@
 use crate::ConfigBuilder;
 
 const HELPFUL: &str = r#"
-;; Adds contextual information to Emacs help documentation, making it
-;; easier to locate code examples and see sample usage when looking
+;; Helpful adds contextual information to Emacs help documentation, making
+;; it easier to locate code examples and see sample usage when looking
 ;; up functions or variables.
 ;; https://github.com/Wilfred/helpful
 (use-package helpful
@@ -16,13 +16,11 @@ const HELPFUL: &str = r#"
 "#;
 
 const VIM: &str = r#"
-;; Adds vim emulation to Emacs. Activating `evil-mode' (which the below does
-;; for all buffers) will swap your keybindings to modal-based vim. That said,
-;; Emacs is a lot more than just editing text. Evil Collection provides a
-;; compatibility layer between Emacs packages and vim keybindings that may not
-;; exist in `evil-mode'. A good example is `dired': with Evil Collection
-;; you can navigate files and directories with vim keybindings instead of the
-;; usual C-p & C-n.
+;; Evil adds vim emulation to Emacs. Activating `evil-mode' will swap your
+;; regular keybindings to modal editing with vim, which this use-package
+;; call configures globally. Since there's a ton of keybindings that Evil
+;; needs to modify it'll occasionally lack support for a particular 
+;; package. Evil Collection fills in those missing gaps.
 ;; https://github.com/emacs-evil/evil
 (use-package evil
   :ensure t
@@ -49,7 +47,7 @@ const DENOTE: &str = r#"
   :ensure t
   :custom
   (denote-known-keywords '("emacs" "journal"))
-  ;; This directory is where your notes live.
+  ;; This is the directory where your notes live.
   (denote-directory (expand-file-name "~/denote/"))
   :bind
   (("C-c n n" . denote)
@@ -58,12 +56,26 @@ const DENOTE: &str = r#"
 "#;
 
 const MAGIT: &str = r#"
-;; Adds a fully-featured git client to Emacs. After working with Magit you'll
+;; Magit is a fully-featured git frontend. After working with Magit you may
 ;; never want to go back to the git CLI. Activate it with `C-c g`.
 ;; https://magit.vc/
 (use-package magit
   :ensure t
   :bind (("C-c g" . magit-status)))
+"#;
+
+const BREADCRUMBS: &str = r#"
+;; You can install packages from version control with use-package and
+;; the :vc keyword argument. For the list of supported fetchers, view
+;; the documentation for the variable `vc-use-package-fetchers'.
+;;
+;; Breadcrumb adds breadcrumbs to the top of your open buffers and works
+;; great with project.el, Emacs's built-in project management package.
+;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Projects.html
+;; https://github.com/joaotavora/breadcrumb
+(use-package breadcrumb
+  :vc (:fetcher github :repo joaotavora/breadcrumb)
+  :init (breadcrumb-mode))
 "#;
 
 pub struct Features {}
@@ -79,6 +91,7 @@ impl ConfigBuilder for Features {
                 "vim" => Some(VIM),
                 "denote" => Some(DENOTE),
                 "magit" => Some(MAGIT),
+                "breadcrumbs" => Some(BREADCRUMBS),
                 _ => None,
             };
 
